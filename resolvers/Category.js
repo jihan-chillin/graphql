be27b7b5ef228e0e0e7bf0cb8c6281e6a40af116
jihan <jihan@jihanui-MacBookPro.local>
2022-05-09 09:16:59 +0900

    graphql query

---
+++ resolvers/Category.js
@@ -0,0 +1,32 @@
+exports.Category = {
+    products : ({id : categoryId}, {filter}, {products}) =>{
+        const categoryProducts = products.filter((product)=>product.categoryId === categoryId);
+        
+        let filteredCategoryProducts = categoryProducts;
+
+        if(filter){
+            const {onSale, avgRating} = filter;
+            if(onSale === true){
+                filteredCategoryProducts = filteredCategoryProducts.filter(product =>{
+                    return product.onSale
+                })
+            }
+            if([1,2,3,4,5].includes(avgRating)){
+                console.log("카테고리 안에 avgRating : ", avgRating);
+                filteredCategoryProducts = filteredCategoryProducts.filter((product)=>{
+                    let sumRating = 0;
+                    let numberOfReviews = 0;
+                    reviews.forEach(review=>{
+                        if(review.productId === product.id) {
+                            sumRating += review.rating;
+                            numberOfReviews++;
+                        }
+                    });
+                    const avgProductRating = sumRating / numberOfReviews;
+                    return avgProductRating >= avgRating;
+                })
+            }
+        }
+        return filteredCategoryProducts;
+    }
+}
